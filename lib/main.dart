8dc87011cd601646ddf75a97e1900a3d938eadf5
ClydeIanVananaz <clyde@vananaz.com>
2022-12-09 20:27:48 +0700

    Formatted codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:k_block_app/widgets/kblock-LoginForm.dart';
-
+import 'package:k_block_app/widgets/login_form.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
       theme: ThemeData(fontFamily: 'NunitoSans'),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home:const Scaffold(
+      home: const Scaffold(
         backgroundColor: Color(0xFFEAEAEA),
-        body:  KBlockLoginForm(),
+        body: KBlockLoginForm(),
       )));
 }
--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -23,30 +23,31 @@ class MyCustomFormState extends State<KBlockLoginForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          const Padding(padding: EdgeInsets.fromLTRB(0,91, 0, 72),
-            child:Center(
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 91, 0, 72),
+            child: Center(
               child: Text('K-BLOCK',
                   style: TextStyle(
                     fontWeight: FontWeight.w700,
                     fontSize: 30,
                     color: Color(0xFF656565),
-                  )
-              ),
+                  )),
             ),
           ),
           // Id Field
-          Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 6),
-            child:Center(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 6),
+            child: Center(
               child: Text(AppLocalizations.of(context)!.id,
                   style: const TextStyle(
                     fontWeight: FontWeight.w400,
                     fontSize: 15,
                     color: Color(0xFF656565),
-                  )
-              ),
+                  )),
             ),
           ),
-          Padding(padding: const EdgeInsets.fromLTRB(34,0, 34, 0),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(34, 0, 34, 0),
               child: SizedBox(
                 height: 40.0,
                 child: TextField(
@@ -56,21 +57,21 @@ class MyCustomFormState extends State<KBlockLoginForm> {
                     ),
                   ),
                 ),
-              )
-          ),
+              )),
           // password field
-          Padding(padding: const EdgeInsets.fromLTRB(0, 10, 0, 6),
-            child:Center(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 6),
+            child: Center(
               child: Text(AppLocalizations.of(context)!.password,
                   style: const TextStyle(
                     fontWeight: FontWeight.w400,
                     fontSize: 15,
                     color: Color(0xFF656565),
-                  )
-              ),
+                  )),
             ),
           ),
-          Padding(padding: const EdgeInsets.fromLTRB(34,0, 34, 0),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(34, 0, 34, 0),
               child: SizedBox(
                 height: 40.0,
                 child: TextField(
@@ -81,35 +82,35 @@ class MyCustomFormState extends State<KBlockLoginForm> {
                     ),
                   ),
                 ),
-              )
-          ),
+              )),
           // forgot password link
-          Padding(padding: const EdgeInsets.fromLTRB(0, 10, 0, 7),
-            child:Center(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 7),
+            child: Center(
               child: Text(AppLocalizations.of(context)!.forgot_password,
                   style: const TextStyle(
                     fontWeight: FontWeight.w400,
                     fontSize: 15,
                     color: Color(0xFF656565),
-                  )
-              ),
+                  )),
             ),
           ),
-          Padding(padding: const EdgeInsets.fromLTRB(34, 30, 34, 7),
-            child:Center(
+          Padding(
+            padding: const EdgeInsets.fromLTRB(34, 30, 34, 7),
+            child: Center(
               child: OutlinedButton(
                 onPressed: null,
                 style: OutlinedButton.styleFrom(
                     side: const BorderSide(color: Color(0xFF898989)),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(40.0),
-                    )
-                ),
+                    )),
                 child: SizedBox(
                   width: double.infinity,
                   height: 40.0,
-                  child: Padding(padding:  const EdgeInsets.fromLTRB(0, 10, 0, 12),
-                      child:  SizedBox(
+                  child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 12),
+                      child: SizedBox(
                           width: double.infinity,
                           child: Text(
                             AppLocalizations.of(context)!.login,
@@ -117,7 +118,7 @@ class MyCustomFormState extends State<KBlockLoginForm> {
                           ))),
                 ),
               ),
-            ) ,
+            ),
           )
         ],
       ),
