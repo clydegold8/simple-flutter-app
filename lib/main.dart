eb3a56fb3651dc0a32b1c242b034fb6eaa87b0ff
ClydeIanVananaz <clyde@vananaz.com>
2022-12-09 20:06:02 +0700

    Added some refactors

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:k_block_app/widgets/kblock-LoginForm.dart';
 
 
 void main() {
   runApp( MaterialApp(
       theme: ThemeData(fontFamily: 'NunitoSans'),
-      localizationsDelegates: AppLocalizations.localizationsDelegates, // <- here
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home:const Scaffold(
         backgroundColor: Color(0xFFEAEAEA),
         body:  KBlockLoginForm(),
       )));
-}
-
-class KBlockLoginForm extends StatefulWidget {
-  const KBlockLoginForm({super.key});
-
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-class MyCustomFormState extends State<KBlockLoginForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Padding(padding: EdgeInsets.fromLTRB(0,91, 0, 72),
-            child:Center(
-              child: Text('K-BLOCK',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 30,
-                    color: Color(0xFF656565),
-                  )
-              ),
-            ),
-          ),
-          // Id Field
-           Padding(padding: const EdgeInsets.fromLTRB(0, 0, 0, 6),
-            child:Center(
-              child: Text(AppLocalizations.of(context)!.id,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    color: Color(0xFF656565),
-                  )
-              ),
-            ),
-          ),
-          Padding(padding: const EdgeInsets.fromLTRB(34,0, 34, 0),
-            child: SizedBox(
-              height: 40.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(40.0),
-                  ),
-                ),
-              ),
-            )
-          ),
-          // password field
-          Padding(padding: const EdgeInsets.fromLTRB(0, 10, 0, 6),
-            child:Center(
-              child: Text(AppLocalizations.of(context)!.password,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    color: Color(0xFF656565),
-                  )
-              ),
-            ),
-          ),
-          Padding(padding: const EdgeInsets.fromLTRB(34,0, 34, 0),
-              child: SizedBox(
-                height: 40.0,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                      borderSide: const BorderSide(color: Color(0xFF898989)),
-                    ),
-                  ),
-                ),
-              )
-          ),
-          // forgot password link
-          Padding(padding: const EdgeInsets.fromLTRB(0, 10, 0, 7),
-            child:Center(
-              child: Text(AppLocalizations.of(context)!.forgot_password,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 15,
-                    color: Color(0xFF656565),
-                  )
-              ),
-            ),
-          ),
-          Padding(padding: const EdgeInsets.fromLTRB(34, 30, 34, 7),
-            child:Center(
-              child: OutlinedButton(
-                onPressed: null,
-                style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Color(0xFF898989)),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                    )
-                ),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 40.0,
-                  child: Padding(padding:  const EdgeInsets.fromLTRB(0, 10, 0, 12),
-                      child:  SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            AppLocalizations.of(context)!.login,
-                            textAlign: TextAlign.center,
-                          ))),
-                ),
-              ),
-            ) ,
-          )
-        ],
-      ),
-    );
-  }
 }
