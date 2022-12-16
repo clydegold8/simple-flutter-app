ff0c50285bf53ffd5de40f01a55763c4640abd9e
Maharlika Alberca <maharlikaalberca@gmail.com>
2022-12-16 09:26:51 +0800

    added initial files for whitelist page

---
+++ lib/src/features/whitelist/presentation/whitelist.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'whitelist_state.dart';
+
+class Whitelist extends StatefulWidget {
+  const Whitelist({super.key});
+
+  @override
+  WhiteListState createState() {
+    return WhiteListState();
+  }
+}
