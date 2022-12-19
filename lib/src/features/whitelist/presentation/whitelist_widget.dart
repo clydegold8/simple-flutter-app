69610273249996c69e4150eef7f1714c84c9f440
Maharlika Alberca <maharlikaalberca@gmail.com>
2022-12-19 16:40:59 +0800

    added minor fix on colors

--- lib/src/features/whitelist/presentation/whitelist_widget.dart
+++ lib/src/features/whitelist/presentation/whitelist_widget.dart
@@ -9,6 +9,18 @@ Widget whitelistWidget(BuildContext context) {
     backgroundColor: const Color(0xFFFFFFFF),
     appBar: AppBar(
       title: Text(whitelistText),
+      backgroundColor: const Color(0xFFFFFFFF),
+      foregroundColor: const Color(0xFF494848),
+      actions: <Widget>[
+        IconButton(
+            icon: const Icon(Icons.more_vert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+      ],
     ),
     body: Column(
       children: [
@@ -22,6 +34,7 @@ Widget whitelistWidget(BuildContext context) {
     floatingActionButton: FloatingActionButton(
         onPressed: null,
         tooltip: whitelistText,
+        backgroundColor: const Color(0xFF14B53D),
         child: const Icon(Icons.add),
       ),);
 }
