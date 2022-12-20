48917739e6048c081ebe918850e75905372d9ef1
Maharlika Alberca <maharlikaalberca@gmail.com>
2022-12-20 17:57:21 +0800

    fixed sample alert pop up

--- lib/src/features/add_whitelist/presentation/add_whitelist.dart
+++ lib/src/features/add_whitelist/presentation/add_whitelist.dart
@@ -2,13 +2,27 @@ import 'package:flutter/material.dart';
 
 Future<void> addWhitelist(BuildContext context) {
   return showDialog(
-           context: context,
-           builder: (BuildContext context) {
-                    return const Expanded(
-                      child: AlertDialog( 
-                        title: Text('empty pop up'),
-                ),
-          );
-       },
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('AlertDialog Title'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: const <Widget>[
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
   );
 }
