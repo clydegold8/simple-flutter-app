a82c9acb872c5969be9abd12095aa04af34810a1
ClydeIanVananaz <clyde@vananaz.com>
2022-12-28 15:06:54 +0800

    Added bug fixes

--- lib/src/features/app_management/presentation/data_traffic_limit.dart
+++ lib/src/features/app_management/presentation/data_traffic_limit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'package:k_block_app/src/constants/colors.dart';
 
 Future<void> dataTrafficLimitDialogueBuilder(BuildContext context) {
