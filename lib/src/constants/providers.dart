41f2981d07936e677a21cd82afe740ed1eba9116
Lloyd John Borres <lloyd@vananaz.com>
2023-01-06 18:06:26 +0800

    Update ad blocker message status when switch on and fix english translation

--- lib/src/constants/providers.dart
+++ lib/src/constants/providers.dart
@@ -2,3 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final widgetPathProvider = StateProvider((ref) => 0);
 final widgetHomePageTabProvider = StateProvider((ref) => 0);
+final isAdBlockerOnProvider = StateProvider((ref) => false);
--- lib/src/features/ad_blocker/presentation/ad_blocker_widget.dart
+++ lib/src/features/ad_blocker/presentation/ad_blocker_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:k_block_app/src/constants/colors.dart';
+import 'package:k_block_app/src/constants/providers.dart';
 
 import 'ad_blocker_control.dart';
 import 'ad_blocker_tab.dart';
 
 Widget adBlockerWidget(BuildContext context, WidgetRef ref) {
+  final isAdBlockerOn = ref.watch(isAdBlockerOnProvider);
+
   return Column(children: [
     Container(
       color: KBlockColors.greenLight,
@@ -14,7 +18,10 @@ Widget adBlockerWidget(BuildContext context, WidgetRef ref) {
         padding: const EdgeInsets.only(top: 15, bottom: 13),
         child: Center(
             child: Text(
-                AppLocalizations.of(context)?.ad_block_suspended ?? '広告ブロック停止中',
+                (isAdBlockerOn
+                    ? AppLocalizations.of(context)?.ad_block_on ?? '広告ブロック中'
+                    : AppLocalizations.of(context)?.ad_block_suspended ??
+                        '広告ブロック停止中'),
                 style: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
