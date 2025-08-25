1bf507bbaaacbd6f6cf9784c4686f4d301ad32fc
ClydeIanVananaz <clyde@vananaz.com>
2025-08-25 09:24:41 +0800

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -79,8 +79,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ed84e1773862013f571c9657a62b35fc",
-"/": "ed84e1773862013f571c9657a62b35fc",
+"index.html": "d9a724dce031591435bfc840686e5419",
+"/": "d9a724dce031591435bfc840686e5419",
 "main.dart.js": "bd55a9c239904fe5197506bb75667c91",
 "manifest.json": "8042d3ff19d2ce47bf4f81d566c14aec",
 "version.json": "cd5f8e4f397b86cb39c7ace6ea433c04"
